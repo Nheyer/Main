0274ec3b0b41f78104a435edd2bd3d288c128d22
Nick Heyer <nheyer@csumb.edu>
2018-09-07 07:05:42 -0700

    update for modern samtools and add parelization

--- fastq_gz__to__csv.sh
+++ fastq_gz__to__csv.sh
@@ -2,7 +2,7 @@
 
 ##### This file should be placed in the directory with all the gziped R1 fastq files you want with R2 in a seperate directory named the same
 ##### The directures should both b in the same directory
-##### the path to the reference you want to use
+##### the path to the reference you want to use then threads
 ##### (should be renamed ref.fasta) and the python script 'temps_to_csv_line.py'
 ##### The user should append the output of this file to a file
 
@@ -12,25 +12,27 @@ mid_buf=' chr1:119989248-120190000 chr1:149328818-149471561 chr1:148600079-14880
 big_buf='chr1:143200001-150600000 chr1:120400001-121700000'
 #######~~~~~~~~~~~~ FOR THE USER TO EDIT ~~~~~~~~~~~~~#######
 regions=$no_buf
-picard="/home/sheepless/picard/build/libs/picard.jar"
+picard="/public/home/nheyer/bin/picard/build/libs/picard.jar"
 ref=$1
+t=$2
 #### startup ####
 echo 'Sample , reads , duplicates , 1q21.1 , 1q21.1 Enrichment , % mapped , %duplicates , unique BCs , reads/unique BC , average insert size , path to histogram '
 # edit this line to put the absolute path to the directory then *.fastq.gz
 for file in *.fastq.gz
  # unzip the files
- do gzip -d $file
- gzip -d ../R2/$file
+ do pigz -p $t -d $file
+ R2=${file%%_R1_*}_R2_${file##*_R1_} 
+ pigz -p $t -d ../R2/$R2
  #### processing ####
- bwa mem -t 7 ref.fasta "${file::-3}"  "../R2/${file::-3}" > temp_1-2.sam
+ bwa mem -t $t $ref "${file::-3}"  "../R2/${R2::-3}" > temp_1-2.sam
  samtools view -Sb temp_1-2.sam > temp_2-2.bam
- samtools sort temp_2-2.bam temp_3-2
+ samtools sort temp_2-2.bam -o temp_3-2.bam
  # use the original file name (without the endings) to name the file and give it a .infoa file ending
  samtools flagstat temp_3-2.bam > "${file::-9}.infoa"
  # create an index then take only the reads that are in the defined regions and put it into a new temp file, then resort and put it into yet another temp file
  samtools index temp_3-2.bam
  samtools view -b temp_3-2.bam $regions > temp_4.bam
- samtools sort temp_4.bam temp_5
+ samtools sort temp_4.bam -o temp_5.bam
  java -jar $picard CollectInsertSizeMetrics I=temp_5.bam O="${file::-9}.info_insert" H="${file::-9}_histogram.pdf"
  # use the original file name (without the endings) to name the flagstat output from data in the bam containing only the regions defined above and give it a .infob file ending
  samtools flagstat temp_5.bam > "${file::-9}.infob"
@@ -43,6 +45,6 @@ for file in *.fastq.gz
  echo "cleaning up" 1>&2
  # remove all the files we made and re zip the fastq file
  rm temp_*
- gzip "${file::-3}"
- gzip "../R2/${file::-3}"
+ pigz -p $t "${file::-3}"
+ pigz -p $t "../R2/${file::-3}"
  done
